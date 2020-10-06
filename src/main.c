f765f0ec536f6a8a484f401d5bd0221887f22618
Luca Sas <sas.luca.alex@gmail.com>
2020-10-06 14:44:40 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
+#include "stdio.h"
+#include "stdbool.h"
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
-#include "stdbool.h"
 
 typedef enum platform_button_state
 {
@@ -80,29 +80,6 @@ static void window_size_callback(GLFWwindow* glfw_window, int width, int height)
     glViewport(0, 0, window_width, window_height);
 }
 
-typedef GLuint gl_shader;
-
-static gl_shader load_shader(const char* vertex_source, const char* fragment_source)
-{
-    GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertex_shader, 1, (const char**) &vertex_source, NULL);
-    glCompileShader(vertex_shader);
-
-    GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragment_shader, 1, (const char**) &fragment_source, NULL);
-    glCompileShader(fragment_shader);
-
-    GLuint shader_program = glCreateProgram();
-    glAttachShader(shader_program, vertex_shader);
-    glAttachShader(shader_program, fragment_shader);
-    glLinkProgram(shader_program);
-
-    glDeleteShader(vertex_shader);
-    glDeleteShader(fragment_shader);
-
-    return shader_program;
-}
-
 int main()
 {
     // GLFW init
@@ -119,129 +96,6 @@ int main()
     gladLoadGL();
     glfwSwapInterval(1);
 
-    // Renderer init
-    glViewport(0, 0, window_width, window_height);
-
-    // Exercise 1
-    #if 0
-    float vertices[] = {
-        -0.5f, +1.0f, +0.0f, // top left
-        +0.0f, +0.0f, +0.0f, // center
-        -1.0f, +0.0f, +0.0f, // mid left
-        +0.5f, +1.0f, +0.0f, // top right
-        +1.0f, +0.0f, +0.0f, // mid right
-    };
-
-    unsigned int indices[] = {  // note that we start from 0!
-        0, 1, 2,   // first triangle
-        1, 3, 4,   // second triangle
-    };
-
-    GLuint vbo = 0;
-    GLuint ebo = 0;
-    GLuint vao = 0;
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
-    {
-        glGenBuffers(1, &vbo);
-        glBindBuffer(GL_ARRAY_BUFFER, vbo);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-        glGenBuffers(1, &ebo);
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-        glVertexAttribPointer(/*attribute location*/ 0, /*floats per vertex*/3, /*data type*/GL_FLOAT, GL_FALSE, /*stride*/3 * sizeof(float), /*offset*/0);
-        glEnableVertexAttribArray(/*attribute location*/ 0);
-    }
-    glBindVertexArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
-    char* vertex_source = "#version 330 core\n"
-                           "layout (location = 0) in vec3 aPos;\n"
-                           "\n"
-                           "void main()\n"
-                           "{\n"
-                           "    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-                           "}\0";
-
-    char* fragment_source = "#version 330 core\n"
-                            "out vec4 FragColor;\n"
-                            "\n"
-                            "void main()\n"
-                            "{\n"
-                            "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-                            "}";
-
-    gl_shader shader_program = load_shader(vertex_source, fragment_source);
-    #endif
-
-    // Exercise 2
-    #if 1
-    char* vertex_source = "#version 330 core\n"
-                           "layout (location = 0) in vec3 aPos;\n"
-                           "\n"
-                           "void main()\n"
-                           "{\n"
-                           "    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-                           "}\0";
-
-    char* fragment_source = "#version 330 core\n"
-                            "out vec4 FragColor;\n"
-                            "\n"
-                            "void main()\n"
-                            "{\n"
-                            "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-                            "}";
-
-    gl_shader shader_program = load_shader(vertex_source, fragment_source);
-
-    float vertices1[] = {
-        -0.5f, +1.0f, +0.0f, // top left
-        +0.0f, +0.0f, +0.0f, // center
-        -1.0f, +0.0f, +0.0f, // mid left
-        //+0.5f, +1.0f, +0.0f, // top right
-        //+1.0f, +0.0f, +0.0f, // mid right
-    };
-
-    GLuint vbo1 = 0;
-    GLuint vao1 = 0;
-    glGenVertexArrays(1, &vao1);
-    glBindVertexArray(vao1);
-    {
-        glGenBuffers(1, &vbo1);
-        glBindBuffer(GL_ARRAY_BUFFER, vbo1);
-        glBufferData(vbo1, sizeof(vertices1), vertices1, GL_STATIC_DRAW);
-
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
-        glEnableVertexAttribArray(0);
-    }
-    glBindVertexArray(0);
-
-    float vertices2[] = {
-        -0.5f, +1.0f, +0.0f, // top left
-        +0.0f, +0.0f, +0.0f, // center
-        -1.0f, +0.0f, +0.0f, // mid left
-        //+0.5f, +1.0f, +0.0f, // top right
-        //+1.0f, +0.0f, +0.0f, // mid right
-    };
-
-    GLuint vbo1 = 0;
-    GLuint vao1 = 0;
-    glGenVertexArrays(1, &vao1);
-    glBindVertexArray(vao1);
-    {
-        glGenBuffers(1, &vbo1);
-        glBindBuffer(GL_ARRAY_BUFFER, vbo1);
-        glBufferData(vbo1, sizeof(vertices1), vertices1, GL_STATIC_DRAW);
-
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
-        glEnableVertexAttribArray(0);
-    }
-    glBindVertexArray(0);
-    #endif
-
     while (!glfwWindowShouldClose(glfw_window))
     {
         glfwSwapBuffers(glfw_window);
@@ -249,17 +103,8 @@ int main()
 
         // Rendering
         {
-            // Exercise 1
-            #if 0
             glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
             glClear(GL_COLOR_BUFFER_BIT);
-            glUseProgram(shader_program);
-            glBindVertexArray(vao);
-            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-            glBindVertexArray(0);
-            #endif
         }
 
         // Input handling
